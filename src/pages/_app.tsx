c7a96d648b58e90e6c2d046e23f717e4eb3d25ed
pietrobelluno <pietrop@outlook.com>
2021-04-18 22:39:24 -0300

    refactor: remove icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,8 +11,6 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Uniplaces Properties</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <meta
           name="description"
           content="A simple project with TypeScript, React, NextJS and Styled Components"
